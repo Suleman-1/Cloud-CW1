bdb61bfc62c64efe34d7062ad14545c8a0118647
Suleman <61390695+Suleman-1@users.noreply.github.com>
2022-08-10 22:27:23 +0500

    Add files via upload

---
+++ code/section3/bash_script3.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Script for metadata for you app
+
+echo "metadata of car object"
+curl https://storage.googleapis.com/storage/v1/b/dev_lab/o/car.jpg
+
+echo "metadata of cycle object"
+curl https://storage.googleapis.com/storage/v1/b/dev_lab/o/cycle.jpg
+
+echo "metadata of bike object"
+curl https://storage.googleapis.com/storage/v1/b/dev_lab/o/bike.jpg
+
